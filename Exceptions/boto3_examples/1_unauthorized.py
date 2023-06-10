34602303bbd47173ba565ed268033c919975d954
Nick Garner <nick@slithersense.com>
2023-06-10 16:15:47 -0700

    20230610-1615 boto3 exceptions

--- Exceptions/boto3_examples/1_unauthorized.py
+++ Exceptions/boto3_examples/1_unauthorized.py
@@ -3,11 +3,9 @@ import boto3
 
 ec2client = boto3.client('ec2', aws_access_key_id ='IDONTKNOW', aws_secret_access_key='NOTSOSECRET', region_name='us-east-2')
 
-
 # The following will cause an unauthorized exception
 # Comment the following after trying it
-#response = ec2client.describe_instances()
-
+response = ec2client.describe_instances()
 
 try:
     response = ec2client.describe_instances()
